8ba19a458d84664d0adf49d936301b061da8f695
liuzhuohao <804121985@qq.com>
2025-01-26 19:23:40 +0800

    feat:前端博客的api

---
+++ ruoyi-ui/src/api/wanfeng/learn/blog.js
@@ -0,0 +1,35 @@
+import request from '@/utils/request'
+
+// 查询博客列表
+export function listBlog() {
+  return request({
+    url: '/wanfeng/learn/blog/list',
+    method: 'get'
+  })
+}
+
+// 新增博客
+export function addBlog(data) {
+  return request({
+    url: '/wanfeng/learn/blog',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改博客
+export function updateBlog(data) {
+  return request({
+    url: '/wanfeng/learn/blog',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除博客
+export function delBlog(blogId) {
+  return request({
+    url: '/wanfeng/learn/blog/' + blogId,
+    method: 'delete'
+  })
+}
