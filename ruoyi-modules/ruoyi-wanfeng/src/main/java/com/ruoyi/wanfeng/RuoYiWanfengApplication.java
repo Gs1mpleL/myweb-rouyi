f0098ee82c0d202826efe5b7a4732ebf9064e9a8
wanfeng <804121985@qq.com>
2025-02-17 20:38:34 +0800

    feat：部署kafka集群，zookeeper集群

--- ruoyi-modules/ruoyi-wanfeng/src/main/java/com/ruoyi/wanfeng/RuoYiWanfengApplication.java
+++ ruoyi-modules/ruoyi-wanfeng/src/main/java/com/ruoyi/wanfeng/RuoYiWanfengApplication.java
@@ -2,6 +2,9 @@ package com.ruoyi.wanfeng;
 
 import com.ruoyi.common.security.annotation.EnableCustomConfig;
 import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import com.ruoyi.wanfeng.kafka.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,9 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableCustomConfig
 @EnableRyFeignClients
-public class RuoYiWanfengApplication {
+public class RuoYiWanfengApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(RuoYiWanfengApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  晚风服务模块启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+
+    @Autowired
+    KafkaProducerService kafkaProducerService;
+    @Override
+    public void run(String... args) throws Exception {
+        kafkaProducerService.sendMessage("Hello, Kafka!");
     }
 }
